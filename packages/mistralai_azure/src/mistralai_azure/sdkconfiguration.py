270db1a6a4b616b8bababd1833276af5b74ff8e6
regression-test-executor <regression-test-executor@speakeasy.com>
2024-09-18 17:03:17 +0000

    openapi-generation@426153e8522a5bbea14dfdb7c57d14ba8297e58d

--- packages/mistralai_azure/src/mistralai_azure/sdkconfiguration.py
+++ packages/mistralai_azure/src/mistralai_azure/sdkconfiguration.py
@@ -29,8 +29,8 @@ class SDKConfiguration:
     language: str = "python"
     openapi_doc_version: str = "0.0.2"
     sdk_version: str = "1.2.2"
-    gen_version: str = "2.415.6"
-    user_agent: str = "speakeasy-sdk/python 1.2.2 2.415.6 0.0.2 mistralai_azure"
+    gen_version: str = "2.417.0"
+    user_agent: str = "speakeasy-sdk/python 1.2.2 2.417.0 0.0.2 mistralai_azure"
     retry_config: OptionalNullable[RetryConfig] = Field(default_factory=lambda: UNSET)
     timeout_ms: Optional[int] = None
 
